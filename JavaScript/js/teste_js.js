2485baa00d8a96c68bb53e58351e53302a26e4c8
Arthur Aparecido <arthurapcraft2@gmail.com>
2025-09-15 09:19:11 -0300

    Feat add: aulas

---
+++ JavaScript/js/teste_js.js
@@ -0,0 +1,23 @@
+function exibe_ferrari(){
+    document.getElementById("ferrari").style.visibility="visible";
+    document.getElementById("subaru").style.visibility="hidden";
+    document.getElementById("cybertruck").style.visibility="hidden";
+}
+
+function exibe_subaru(){
+    document.getElementById("ferrari").style.visibility="hidden";
+    document.getElementById("subaru").style.visibility="visible";
+    document.getElementById("cybertruck").style.visibility="hidden";
+}
+
+function exibe_cybertruck(){
+    document.getElementById("ferrari").style.visibility="hidden";
+    document.getElementById("subaru").style.visibility="hidden";
+    document.getElementById("cybertruck").style.visibility="visible";
+}
+
+function some_tudo(){
+    document.getElementById("ferrari").style.visibility="hidden";
+    document.getElementById("subaru").style.visibility="hidden";
+    document.getElementById("cybertruck").style.visibility="hidden";
+}
