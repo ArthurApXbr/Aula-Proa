5114090e466d29f591aec68c18b4e934708be0f4
Arthur Aparecido <arthurapcraft2@gmail.com>
2025-09-15 11:00:07 -0300

    add modal

---
+++ JavaScript/js/modals.js
@@ -0,0 +1,9 @@
+function abrir_modal(){
+    let modal = document.querySelector(".modal");
+    let janela_escura = document.querySelector(".janela-escura");
+    let imagem = document.querySelector(".imagem");
+
+    modal.style.visibility="visible";
+    janela_escura.style.visibility="visible";
+    imagem.style.visibility="visible";
+}
